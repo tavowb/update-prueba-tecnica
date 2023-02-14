16c5e13fd84e045903e8f2c69dd6b5aa51ad1823
Gustavo Jimenez <gusweb71@gmail.com>
2023-02-14 16:34:10 -0500

    Agrergo PWA con el pluggin vite-plugin-pwa

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import ReloadPrompt from './ReloadPrompt'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ReloadPrompt />
     <App />
   </React.StrictMode>
 )
